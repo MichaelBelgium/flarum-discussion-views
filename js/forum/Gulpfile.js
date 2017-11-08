d543174ad67ca7d7b5b3de72b4fadb0aa49350e5
Michael V <michaelvrld@gmail.com>
2017-11-08 21:45:45 +0100

    Update gulp and flarum-gulp version

--- js/forum/Gulpfile.js
+++ js/forum/Gulpfile.js
@@ -2,8 +2,6 @@ var flarum = require('flarum-gulp');
 
 flarum({
   modules: {
-    'michaelbelgium/flarum-discussion-views': [
-      'src/**/*.js'
-    ]
+    'michaelbelgium/flarum-discussion-views': 'src/**/*.js'
   }
 });
