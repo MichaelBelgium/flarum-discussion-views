ac41ced1f7f96396885089485986db6b04a3a74d
Michael V <michaelvrld@gmail.com>
2017-11-09 21:49:14 +0100

    Show views in discussionlist

--- bootstrap.php
+++ bootstrap.php
@@ -2,10 +2,12 @@
 
 require_once "listeners/DiscussionWasViewed.php";
 require_once "listeners/AddAssets.php";
+require_once "listeners/AddDiscussionApiAttributes.php";
 
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->subscribe(DiscussionWasViewed::class);
     $events->subscribe(AddAssets::class);
+    $events->subscribe(AddDiscussionApiAttributes::class);
 };
--- js/forum/dist/extension.js
+++ js/forum/dist/extension.js
@@ -1,9 +1,9 @@
 'use strict';
 
-System.register('michaelbelgium/flarum-discussion-views/main', ['flarum/app', 'flarum/extend', 'flarum/components/DiscussionListItem'], function (_export, _context) {
+System.register('michaelbelgium/flarum-discussion-views/main', ['flarum/app', 'flarum/extend', 'flarum/components/DiscussionListItem', 'flarum/Model', 'flarum/models/Discussion'], function (_export, _context) {
     "use strict";
 
-    var app, extend, DiscussionListItem;
+    var app, extend, DiscussionListItem, Model, Discussion;
     return {
         setters: [function (_flarumApp) {
             app = _flarumApp.default;
@@ -11,13 +11,19 @@ System.register('michaelbelgium/flarum-discussion-views/main', ['flarum/app', 'f
             extend = _flarumExtend.extend;
         }, function (_flarumComponentsDiscussionListItem) {
             DiscussionListItem = _flarumComponentsDiscussionListItem.default;
+        }, function (_flarumModel) {
+            Model = _flarumModel.default;
+        }, function (_flarumModelsDiscussion) {
+            Discussion = _flarumModelsDiscussion.default;
         }],
         execute: function () {
 
             app.initializers.add('michaelbelgium-flarum-discussion-views', function () {
+                Discussion.prototype.views = Model.attribute('views');
+
                 extend(DiscussionListItem.prototype, 'infoItems', function (items) {
                     var discussion = this.props.discussion;
-                    items.add('discussion-views', discussion.data.id);
+                    items.add('discussion-views', discussion.views());
                 });
             });
         }
