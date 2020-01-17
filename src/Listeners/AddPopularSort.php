caec6c1c1679cfdde9033f1869d43cf1b3b1aeac
Michael V <michaelvrld@gmail.com>
2020-01-17 12:25:59 +0100

    Implement setting abbr numbers + enable filters

--- src/Listeners/AddPopularSort.php
+++ src/Listeners/AddPopularSort.php
@@ -3,15 +3,24 @@ namespace Michaelbelgium\Discussionviews\Listeners;
 
 use Illuminate\Contracts\Events\Dispatcher;
 use Flarum\Api\Event\WillGetData;
+use Flarum\Settings\SettingsRepositoryInterface;
 
 class AddPopularSort
 {
+    private $settings;
+
+    public function __construct(SettingsRepositoryInterface $settings) {
+        $this->settings = $settings;
+    }
+
     /**
      * @param Dispatcher $events
      */
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(WillGetData::class, [$this, 'confApi']);
+        if($this->settings->get('michaelbelgium-discussionviews.show_filter', true)) {
+            $events->listen(WillGetData::class, [$this, 'confApi']);
+        }
     }
 
     /**
