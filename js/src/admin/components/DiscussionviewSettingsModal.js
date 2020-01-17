337d10d86a46a982b6904b343cbd9b5636694bf0
Michael V <michaelvrld@gmail.com>
2020-01-17 12:15:44 +0100

    Fix switch state issue

--- js/src/admin/components/DiscussionviewSettingsModal.js
+++ js/src/admin/components/DiscussionviewSettingsModal.js
@@ -1,6 +1,5 @@
 import SettingsModal from 'flarum/components/SettingsModal';
 import Switch from 'flarum/components/Switch';
-import FieldSet from 'flarum/components/FieldSet';
 
 export default class DiscussionviewSettingsModal extends SettingsModal {
     className() {
@@ -15,19 +14,19 @@ export default class DiscussionviewSettingsModal extends SettingsModal {
         return [
             <div className="Form-group">
                 {Switch.component({
-                    state: this.setting('michaelbelgium-discussionviews.track_unique')(),
+                    state: this.setting('michaelbelgium-discussionviews.track_unique')() == 1,
                     onchange: this.setting('michaelbelgium-discussionviews.track_unique'),
                     children: app.translator.trans('flarum_discussion_views.admin.settings.track_unique')
                 })}
                 
                 {Switch.component({
-                    state: this.setting('michaelbelgium-discussionviews.abbr_numbers')(),
+                    state: this.setting('michaelbelgium-discussionviews.abbr_numbers')() == 1,
                     onchange: this.setting('michaelbelgium-discussionviews.abbr_numbers'),
                     children: app.translator.trans('flarum_discussion_views.admin.settings.abbr_numbers')
                 })}
                 
                 {Switch.component({
-                    state: this.setting('michaelbelgium-discussionviews.show_filter')(),
+                    state: this.setting('michaelbelgium-discussionviews.show_filter')() == 1,
                     onchange: this.setting('michaelbelgium-discussionviews.show_filter'),
                     children: app.translator.trans('flarum_discussion_views.admin.settings.show_filter')
                 })}
