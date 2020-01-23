3136d3664e31765cfd5ef5ac6b3b10446d3957c0
Michael V <michaelvrld@gmail.com>
2020-01-23 22:19:42 +0100

    Fix issue on navigating back to discussion list
    Fixes #17

--- js/src/forum/components/AddViewsToModelAndDisplay.js
+++ js/src/forum/components/AddViewsToModelAndDisplay.js
@@ -2,6 +2,7 @@ import { extend } from 'flarum/extend';
 import Model from 'flarum/Model';
 import Discussion from 'flarum/models/Discussion';
 import DiscussionListItem from 'flarum/components/DiscussionListItem';
+import DiscussionList from 'flarum/components/DiscussionList';
 import abbreviateNumber from 'flarum/utils/abbreviateNumber';
 import DiscussionView from '../models/DiscussionView';
 
@@ -17,4 +18,8 @@ export default function () {
         var number = app.forum.attribute('mb-discussionviews.abbr_numbers') == 1 ? abbreviateNumber(discussion.views().length) : discussion.views().length;
         items.add('discussion-views', number);
     });
+
+    extend(DiscussionList.prototype, 'requestParams', function(params) {
+        params.include.push('views'); //fixes not loading relationship when navigating back to the discussion list if you directly went to a discussion or another page
+    })
 }
