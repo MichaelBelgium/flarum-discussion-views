c2a7709f66ab6bc37af0ca4f5c5cc10f2e6d43af
Michael V <michaelvrld@gmail.com>
2020-05-21 15:34:18 +0200

    Use new model extender

--- extend.php
+++ extend.php
@@ -1,9 +1,17 @@
 <?php
 
+use Flarum\Database\AbstractModel;
+use Flarum\Discussion\Discussion;
 use Michaelbelgium\Discussionviews\Listeners;
 use Illuminate\Contracts\Events\Dispatcher;
 use Flarum\Extend\Locales;
 use Flarum\Extend\Frontend;
+use Flarum\Extend\Model;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Michaelbelgium\Discussionviews\Listeners\AddRelationship;
+use Michaelbelgium\Discussionviews\Models\DiscussionView;
+
+$settings = app(SettingsRepositoryInterface::class);
 
 return [
     (new Frontend('forum'))
@@ -14,6 +22,12 @@ return [
         ->js(__DIR__ . '/js/dist/admin.js'),
 
     new Locales(__DIR__ . '/locale'),
+
+    (new Model(Discussion::class))->relationship(AddRelationship::RELATIONSHIP, function (AbstractModel $model) {
+        return $model->hasMany(DiscussionView::class)->orderBy('visited_at', 'DESC');
+    })->relationship(AddRelationship::RELATIONSHIP_LATEST, function (AbstractModel $model) use ($settings) {
+        return $model->views()->limit($settings->get('michaelbelgium-discussionviews.max_listcount', 5));
+    }),
 
     function (Dispatcher $events) {
         $events->subscribe(Listeners\AddRelationship::class);
--- src/Listeners/AddRelationship.php
+++ src/Listeners/AddRelationship.php
@@ -5,12 +5,8 @@ namespace Michaelbelgium\Discussionviews\Listeners;
 use Flarum\Api\Event\WillGetData;
 use Flarum\Api\Serializer\DiscussionSerializer;
 use Flarum\Api\Event\Serializing;
-use Flarum\Discussion\Discussion;
 use Flarum\Event\GetApiRelationship;
-use Flarum\Event\GetModelRelationship;
-use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Events\Dispatcher;
-use Michaelbelgium\Discussionviews\Models\DiscussionView;
 use Michaelbelgium\Discussionviews\Serializers\DiscussionViewSerializer;
 
 class AddRelationship
@@ -18,32 +14,14 @@ class AddRelationship
     const RELATIONSHIP = 'views'; //$discussion->views()
     const RELATIONSHIP_LATEST = 'latestViews';
 
-    private $settings;
-
-    public function __construct(SettingsRepositoryInterface $settings) {
-        $this->settings = $settings;
-    }
-
     /**
      * @param Dispatcher $events
      */
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(GetModelRelationship::class, [$this, "addRelationship"]);
         $events->listen(GetApiRelationship::class, [$this, 'addApiRelationship']);
         $events->listen(WillGetData::class, [$this, 'includeRelationship']);
         $events->listen(Serializing::class, [$this, 'prepareApiAttributes']);
-    }
-
-    public function addRelationship(GetModelRelationship $event)
-    {
-        if($event->isRelationship(Discussion::class, self::RELATIONSHIP)) {
-            return $event->model->hasMany(DiscussionView::class)->orderBy('visited_at', 'DESC');
-        }
-
-        if($event->isRelationship(Discussion::class, self::RELATIONSHIP_LATEST)) {
-            return $event->model->views()->limit($this->settings->get('michaelbelgium-discussionviews.max_listcount', 5));
-        }
     }
 
     public function addApiRelationship(GetApiRelationship $event)
