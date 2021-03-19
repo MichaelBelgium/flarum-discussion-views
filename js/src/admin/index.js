f3fd1b6ce37e68a9068a5b87ff60252b77e193ce
Michael V <michaelvrld@gmail.com>
2021-03-19 18:17:37 +0100

    Update admin javascript
    - Use registerSetting and registerPermission
    - Remove settingsmodal

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,25 +1,35 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
-import PermissionGrid from 'flarum/components/PermissionGrid';
-import DiscussionviewSettingsModal from './components/DiscussionviewSettingsModal';
 
-app.initializers.add('michaelbelgium-admin-discussion-views', () => {
-	extend(PermissionGrid.prototype, 'moderateItems', items => {
-		items.add('resetViews', {
-			icon: 'far fa-eye',
-			label: app.translator.trans('michaelbelgium-discussion-views.admin.permissions.reset_views_label'),
-			permission: 'discussion.resetViews'
-		});
-	});
-
-	extend(PermissionGrid.prototype, 'viewItems', items => {
-		items.add('viewDiscussionNumber', {
-			icon: 'far fa-eye',
-			label: app.translator.trans('michaelbelgium-discussion-views.admin.permissions.view_number_label'),
-			permission: 'discussion.readViewnumber',
-			allowGuest: true
-		});
-	});
-
-	app.extensionSettings['michaelbelgium-discussion-views'] = () => app.modal.show(DiscussionviewSettingsModal);
+app.initializers.add('michaelbelgium-admin-discussion-views', (app) => {
+    app.extensionData.for('michaelbelgium-discussion-views')
+        .registerSetting({
+            setting: 'michaelbelgium-discussionviews.track_unique',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.track_unique_label'),
+            type: 'boolean'
+        }).registerSetting({
+            setting: 'michaelbelgium-discussionviews.abbr_numbers',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.abbr_numbers_label'),
+            type: 'boolean'
+        }).registerSetting({
+            setting: 'michaelbelgium-discussionviews.show_filter',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_filter_label'),
+            type: 'boolean'
+        }).registerSetting({
+            setting: 'michaelbelgium-discussionviews.show_viewlist',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_viewlist_label'),
+            type: 'boolean'
+        }).registerSetting({
+            setting: 'michaelbelgium-discussionviews.max_listcount',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.max_viewcount_label'),
+            type: 'number'
+        }).registerPermission({
+            icon: 'far fa-eye',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.permissions.reset_views_label'),
+            permission: 'discussion.resetViews'
+        }, 'moderate').registerPermission({
+            icon: 'far fa-eye',
+            label: app.translator.trans('michaelbelgium-discussion-views.admin.permissions.view_number_label'),
+            permission: 'discussion.readViewnumber',
+            allowGuest: true
+        }, 'view');
 });
