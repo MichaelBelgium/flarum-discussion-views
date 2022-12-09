ae950d886bd4ee98dfa6160dd075a644441af8dd
Michael V <michaelvrld@gmail.com>
2022-12-09 22:18:49 +0100

    Add help texts to most settings

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -5,11 +5,13 @@ app.initializers.add('michaelbelgium-admin-discussion-views', (app) => {
         .registerSetting({
             setting: 'michaelbelgium-discussionviews.track_unique',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.track_unique_label'),
-            type: 'boolean'
+            type: 'boolean',
+            help: app.translator.trans('michaelbelgium-discussion-views.admin.settings.track_unique_text')
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.ignore_crawlers',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.ignore_crawlers_label'),
-            type: 'boolean'
+            type: 'boolean',
+            help: app.translator.trans('michaelbelgium-discussion-views.admin.settings.ignore_crawlers_text')
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.track_guests',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.track_guests_label'),
@@ -17,7 +19,8 @@ app.initializers.add('michaelbelgium-admin-discussion-views', (app) => {
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.abbr_numbers',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.abbr_numbers_label'),
-            type: 'boolean'
+            type: 'boolean',
+            help: app.translator.trans('michaelbelgium-discussion-views.admin.settings.abbr_numbers_text')
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.show_filter',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_filter_label'),
@@ -25,11 +28,13 @@ app.initializers.add('michaelbelgium-admin-discussion-views', (app) => {
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.show_viewlist',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_viewlist_label'),
-            type: 'boolean'
+            type: 'boolean',
+            help: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_viewlist_text')
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.show_footer_viewlist',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_footer_viewlist_label'),
-            type: 'boolean'
+            type: 'boolean',
+            help: app.translator.trans('michaelbelgium-discussion-views.admin.settings.show_footer_viewlist_text')
         }).registerSetting({
             setting: 'michaelbelgium-discussionviews.max_listcount',
             label: app.translator.trans('michaelbelgium-discussion-views.admin.settings.max_viewcount_label'),
